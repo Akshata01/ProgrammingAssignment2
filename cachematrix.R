7a383b046c02324a661b99f4ec84b4ba1ed4ef10
Akshata01 <akshata.chitakudige@gmail.com>
2014-10-25 22:47:52 +0800

    Committing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functione below are used to catche the inverse of a matrix.
+## Use: Computing the inverse of a matrix is computationally intensive and hence
+## it makes sense to catche the inverse value, especially for large matrices.
 
-## Write a short comment describing this function
+## This function does gets and sets the value of the matrix.
+## It also gets and sets the value of the matrix inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function checks if the matrix inverse is already catched. 
+## If so, it picks up the value from the catch. 
+## Otherwise, it computes the new inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
 }
+
+
